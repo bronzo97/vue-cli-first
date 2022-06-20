69c7ad12f21b9aed2132ee68543d56e395adbdca
bronzo97 <danilobronzo97@hotmail.it>
2022-06-20 17:50:53 +0200

    exercise done

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <div>
-    <h1>La mia prima app con Vue CLI!</h1>
-  </div>
+<div>
+    <TitleT></TitleT>
+</div>  
 </template>
 
 <script>
+import TitleT from './components/TitleT.vue'
 // import HelloWorld from './components/HelloWorld.vue'
 
-// export default {
-//   name: 'App',
-//   components: {
-//     HelloWorld
-//   }
-// }
+export default {
+  name: 'App',
+  components: {
+//     HelloWorld,
+    TitleT,
+  }
+}
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
